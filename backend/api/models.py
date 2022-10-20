f3b3a735b1cfb55b8e0abeb8b05465a48b0bacf1
azuma <azuma20011224@gmail.com>
2022-10-20 23:26:31 +0900

    fix database

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,9 +7,9 @@ def upload_avatar_path(instance, filename):
     ext = filename.split('.')[-1]
     return '/'.join(['avatars', str(instance.userProfile.id)+str(instance.name)+str(".")+str(ext)])
 
-def upload_animal_path(instance, filename):
+def upload_post_path(instance, filename):
     ext = filename.split('.')[-1]
-    return '/'.join(['animal', str(instance.animal.id)+str(instance.name)+str(".")+str(ext)])
+    return '/'.join(['post', str(instance.post.id)+str(instance.title)+str(".")+str(ext)])
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
@@ -62,33 +62,20 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-    SEX_CHOICES = (
-        (1, 'オス'),
-        (2, 'メス'),
-    )
-
-    animal = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name='animal',
+    post = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='post',
         on_delete=models.CASCADE
     )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(verbose_name='名前', max_length=50)
+    title = models.CharField(verbose_name='タイトル', max_length=50)
     description = models.TextField(verbose_name='概要', max_length=1000, blank=True, null=True)
-    img = models.ImageField(verbose_name='画像', blank=True, null=True, upload_to=upload_animal_path)
-    age = models.IntegerField(verbose_name='年齢')
-    kind = models.CharField(verbose_name='品種', max_length=50, blank=True, null=True)
-    sex = models.IntegerField(verbose_name='性別', choices=SEX_CHOICES)
-    birthDate = models.DateField(verbose_name='生年月日')
-    birthPlace = models.CharField(verbose_name='出生地', max_length=50)
-    height = models.IntegerField(verbose_name='身長')
-    weight = models.IntegerField(verbose_name='体重')
-    kategory = models.CharField(verbose_name='カテゴリー', max_length=50)
+    img = models.ImageField(verbose_name='画像', blank=True, null=True, upload_to=upload_post_path)
     created_at = models.DateTimeField(verbose_name='作成日', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True)
     liked = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked',blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class Comment(models.Model):
@@ -97,7 +84,7 @@ class Comment(models.Model):
         settings.AUTH_USER_MODEL, related_name='comment',
         on_delete=models.CASCADE
     )
-    animal = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(verbose_name='作成日', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True)
 
